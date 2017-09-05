ebb81de39930cd09152dc5fe94a32df8a7e40e4f
chris-pyle <christopher.pyle@hdrinc.com>
2017-09-05 15:58:49 -0700

    wip - Stubbed out Sharepoint Parcel Docs Crud - untested, need to finish Doc Exists and Doc Insert methods

---
+++ SharePointInterface/SharePointCRUD.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SharePointInterface
+{
+    class SharePointCRUD
+    {
+        private ClientContext _ctx;
+        private string _parcelsFolderName;
+        public SharePointCRUD ()
+        {
+            _ctx = new ClientContext("http://SiteUrl");
+            _parcelsFolderName = "parcels";
+        }
+
+        // Upload Parcel Doc
+        public bool UploadParcelDoc(string pid, string docType, string docName, byte[] docBytes)
+        {
+            // Get Parcel folder list
+            Web web = _ctx.Web;
+            List parcelFolders = web.Lists.GetByTitle(_parcelsFolderName);
+
+            // Check if Parcel & Doc Type Folder Exists
+            List<string> targetPath = new List<string>() { pid, docType };
+            Folder docFolder = EnsureAndGetTargetFolder(_ctx, parcelFolders, targetPath);
+            
+            // Check if Doc exists
+
+            bool _docExists = false;
+            return _docExists;
+        }
+
+        // Doc Exists
+
+        // Insert Doc
+
+        /// <summary>
+        /// Will ensure nested folder creation if folders in folderPath don't exist.
+        /// </summary>
+        /// <param name="ctx">Loaded SharePoint Client Context</param>
+        /// <param name="list">Document Library SharePoint List Object</param>
+        /// <param name="folderPath">List of strings ParentFolder, ChildFolder, ...</param>
+        /// <returns>Last ChildFolder as target</returns>
+        private static Folder EnsureAndGetTargetFolder(ClientContext ctx, List list, List<string> folderPath)
+        {
+            Folder returnFolder = list.RootFolder;
+            if (folderPath != null && folderPath.Count > 0)
+            {
+                Web web = ctx.Web;
+                Folder currentFolder = list.RootFolder;
+                ctx.Load(web, t => t.Url);
+                ctx.Load(currentFolder);
+                ctx.ExecuteQuery();
+                foreach (string folderPointer in folderPath)
+                {
+                    FolderCollection folders = currentFolder.Folders;
+                    ctx.Load(folders);
+                    ctx.ExecuteQuery();
+
+                    bool folderFound = false;
+                    foreach (Folder existingFolder in folders)
+                    {
+                        if (existingFolder.Name.Equals(folderPointer, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            folderFound = true;
+                            currentFolder = existingFolder;
+                            break;
+                        }
+                    }
+
+                    if (!folderFound)
+                    {
+                        ListItemCreationInformation itemCreationInfo = new ListItemCreationInformation();
+                        itemCreationInfo.UnderlyingObjectType = FileSystemObjectType.Folder;
+                        itemCreationInfo.LeafName = folderPointer;
+                        itemCreationInfo.FolderUrl = currentFolder.ServerRelativeUrl;
+                        ListItem folderItemCreated = list.AddItem(itemCreationInfo);
+                        folderItemCreated.Update();
+                        ctx.Load(folderItemCreated, f => f.Folder);
+                        ctx.ExecuteQuery();
+                        currentFolder = folderItemCreated.Folder;
+                    }
+                }
+                returnFolder = currentFolder;
+            }
+            return returnFolder;
+        }
+    }
+}
