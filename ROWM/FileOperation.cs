46a6759b43f435abe3ec8fb6ea0b21a3b5fa34c9
Kelly Chan <tui.chan@hotmail.com>
2017-09-21 21:00:02 +0000

    fileoperation in both swaggers

---
+++ ROWM/FileOperation.cs
@@ -0,0 +1,29 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ROWM
+{
+    // swagger helper
+    public class FileOperation : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.OperationId.Equals("ApiParcelsByPidDocumentsPost"))
+            {
+                operation.Parameters.Add(new NonBodyParameter
+                {
+                    Name = "File",
+                    In = "formData",
+                    Description = "documents",
+                    Required = true,
+                    Type = "file"
+                });
+                operation.Consumes.Add("multipart/form-data");
+            }
+        }
+    }
+}
