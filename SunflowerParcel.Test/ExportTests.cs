05eaebc102838dbe1c9cfddbda57d7ec50b07880
Kelly Chan <kelly.chan@hdrinc.com>
2017-10-20 04:26:55 +0000

    add a "de-normalized" contacts export

---
+++ SunflowerParcel.Test/ExportTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ROWM.Controllers;
+
+namespace SunflowerParcel.Test
+{
+    [TestClass]
+    public class ExportTests
+    {
+        [TestMethod]
+        public void Test_Export_Contacts()
+        {
+            var rep = new ROWM.Dal.OwnerRepository();
+            var ctrl = new ExportController(rep);
+
+            dynamic file = ctrl.ExportContact("excel");
+            Assert.IsNotNull(file);
+
+            System.IO.File.WriteAllBytes(@"D:/junk/contacts.csv", file.FileContents);
+        }
+    }
+}
