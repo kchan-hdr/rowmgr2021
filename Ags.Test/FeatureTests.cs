8ff58dfbbd3c992b91161bef1f98a0eb5ab61cd2
Kelly Chan <kelly.chan@hdrinc.com>
2018-10-03 06:10:06 +0000

    secured arcgis server

--- Ags.Test/FeatureTests.cs
+++ Ags.Test/FeatureTests.cs
@@ -17,7 +17,26 @@ namespace Ags.Test
             var parcels = await h.GetAllParcels();
 
             Assert.IsNotNull(parcels);
-            Assert.AreEqual(67, parcels.Count());
+            Assert.AreEqual(76, parcels.Count());
+
+            foreach (var p in parcels)
+            {
+                Trace.WriteLine($"{p.OBJECTID}, {p.ParcelId}");
+                Assert.IsFalse(string.IsNullOrWhiteSpace(p.ParcelId));
+            }
+        }
+
+        [TestMethod, TestCategory("AGS")]
+        public async Task Locked_down_sunflower()
+        {
+            var h = new SunflowerParcel("https://gis05s.hdrgateway.com/arcgis/rest/services/California/Sunflower_Parcel_Stg_FS/FeatureServer");
+            var parcels = await h.GetAllParcels();
+
+            var (t, d) = await h.Token();
+            Trace.WriteLine($"token {t} expiration {d}");
+
+            Assert.IsNotNull(parcels);
+            Assert.AreEqual(255, parcels.Count());
 
             foreach (var p in parcels)
             {
---
+++ Ags.Test/TokenTests.cs
@@ -0,0 +1,45 @@
+﻿
+using geographia.ags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ags.Test
+{
+    [TestClass]
+    public class TokenTests
+    {
+        [TestMethod, TestCategory("AGS")]
+        public async Task Normal_Token_Gen()
+        {
+            var server = "http://gis05s.hdrgateway.com/arcgis/rest/services/California/B2H_ROW_Parcels_FS_stg/FeatureServer";
+            var h = new TokenHelper(server);
+            var good = await h.Validate();
+            Assert.IsTrue(good);
+
+            var token = await h.GetToken();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(token));
+        }
+
+        [TestMethod, TestCategory("AGS"), ExpectedException(typeof(KeyNotFoundException))]
+        public async Task Bad_Service_Url()
+        {
+            var server = "http://gis05s.hdrgateway.com/arcgis/rest";
+            var h = new TokenHelper(server);
+            var token = await h.GetToken();
+        }
+
+        [TestMethod, TestCategory("AGS"), ExpectedException(typeof(ApplicationException))]
+        public async Task Bad_Service()
+        {
+            var server = "http://gis05s.hdrgateway.com/arcgis/rest/services/California/B2H_ROW_Parcels_FS_stgg/FeatureServer";
+            var h = new TokenHelper(server);
+            var bad = await h.Validate();
+
+            Assert.IsFalse(bad);
+        }
+    }
+}
