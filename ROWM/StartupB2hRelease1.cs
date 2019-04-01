e2927e6bffed5fbdb35c7fe56ac10b5b9fc8a49d
Kelly Chan <kchan@hdrinc.com>
2019-04-01 06:49:16 +0000

    creates a specific b2h release config

---
+++ ROWM/StartupB2hRelease1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Http.Features;
+using geographia.ags;
+using SharePointInterface;
+
+namespace ROWM
+{
+    public class StartupB2hRelease1
+    {
+        public StartupB2hRelease1(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            // Add framework services.
+            services.AddMvc()
+                .AddJsonOptions(o =>
+                {
+                    o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+
+            services.Configure<FormOptions>(o =>
+            {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+            });
+
+            var cs = Configuration.GetConnectionString("ROWM_Context");
+            //services.AddScoped<ROWM.Dal.ROWM_Context>();
+            services.AddScoped<ROWM.Dal.ROWM_Context>(fac =>
+            {
+               return new Dal.ROWM_Context(cs);
+            });
+
+            services.AddScoped<ROWM.Dal.OwnerRepository>();
+            services.AddScoped<ROWM.Dal.StatisticsRepository>();
+            services.AddScoped<ROWM.Dal.AppRepository>();
+            services.AddScoped<ROWM.Dal.DocTypes>(fac => new Dal.DocTypes(new Dal.ROWM_Context(cs)));
+            services.AddScoped<Controllers.ParcelStatusHelper>();
+            services.AddScoped<IFeatureUpdate, B2hParcel>( fac => 
+                new B2hParcel("https://gis05.hdrgateway.com/arcgis/rest/services/California/B2H_ROW_Parcel_FS/FeatureServer")
+            );
+            services.AddScoped<ISharePointCRUD, SharePointCRUD>();
+
+            services.AddSingleton<SiteDecoration, B2H>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "ROW Manager", Version = "v1" });
+            });
+            services.ConfigureSwaggerGen(o =>
+           {
+               o.OperationFilter<FileOperation>();
+           });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseExceptionHandler("/Home/Error");
+ 
+            app.UseStaticFiles();
+
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ROW Manager V1");
+            });
+        }
+    }
+}
