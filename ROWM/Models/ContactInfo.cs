69cd0ad8cfb651d969910bc17bc4760ffaed6d8a
tui chan <tui.chan@hotmail.com>
2020-06-10 17:31:42 -0700

    db bug fix

---
+++ ROWM/Models/ContactInfo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.KeyVault.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ROWM.Dal
+{
+    public partial class ContactInfo2 : ContactInfo
+    {
+        public bool DoNotEmail { get; set; } = false;
+        public bool DoNotLetter { get; set; } = false;
+        public bool DoNotCallHomePhone { get; set; } = false;
+        public bool DoNotCallWorkPhone { get; set; } = false;
+        public bool DoNotCallCellPhone { get; set; } = false;
+        public string PreferredContactMode { get; set; }
+    }
+}
