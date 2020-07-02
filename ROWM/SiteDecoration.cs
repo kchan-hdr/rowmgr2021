b6cc4810a92f479de3bc21aa1b127bfc59b75b3e
Kelly Chan <kchan@hdrinc.com>
2020-07-01 18:19:30 -0700

    rubbish edits to exercise the deployment process

--- ROWM/SiteDecoration.cs
+++ ROWM/SiteDecoration.cs
@@ -42,6 +42,6 @@
 
     public class Uinta : SiteDecoration
     {
-        public string SiteTitle() => "Uinta Basin Railway (pre-release)";
+        public string SiteTitle() => "Uinta Basin Railway (2020.7.1)";
     }
 }
