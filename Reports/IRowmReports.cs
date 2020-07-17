1f9814cf17bc1412c4c922e7ddcad3c5c019ddd8
tui chan <tui.chan@hotmail.com>
2020-07-16 23:05:36 -0700

    add wharton at-a-glance

---
+++ Reports/IRowmReports.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROWM.Reports
+{
+    public interface IRowmReports
+    {
+        IEnumerable<ReportDef> GetReports();
+        Task<ReportPayload> GenerateReport(ReportDef d);
+    }
+
+    public class ReportDef
+    {
+        public string ReportCode { get; set; }
+        public string Caption { get; set; }
+        public string ReportUrl { get; set; }
+        public int DisplayOrder { get; set; }
+        public bool SupportIndividuals { get; set; } = false;
+    }
+
+    public class ReportPayload
+    {
+        public string Filename { get; set; }
+        public string Mime { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
