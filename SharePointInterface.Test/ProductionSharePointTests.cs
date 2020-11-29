157dea32c9376ca090f94d299ec0a6360626d47c
Kelly Chan <kelly.chan@hdrinc.com>
2020-11-29 02:49:41 +0000

    add atc tests

--- SharePointInterface.Test/ProductionSharePointTests.cs
+++ SharePointInterface.Test/ProductionSharePointTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SharePointInterface.Test
@@ -20,6 +22,31 @@ namespace SharePointInterface.Test
             Assert.AreNotEqual(0, lists.Count);
 
             foreach( var l in lists)
+            {
+                Trace.WriteLine(l.Title);
+            }
+        }
+
+        [TestMethod, TestCategory("SharePointCRUD")]
+        public void TestConnection_ATC()
+        {
+            var msi = new AzureServiceTokenProvider();
+            var vaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(msi.KeyVaultTokenCallback));
+            var appid = vaultClient.GetSecretAsync("https://atc-rowm-key.vault.azure.net/", "atc-client").GetAwaiter().GetResult();
+            var apps = vaultClient.GetSecretAsync("https://atc-rowm-key.vault.azure.net/", "atc-secret").GetAwaiter().GetResult();
+            //services.AddScoped<ISharePointCRUD, SharePointCRUD>(fac => new SharePointCRUD(
+            //   d: fac.GetRequiredService<DocTypes>(), __appId: appid.Value, __appSecret: apps.Value, _url: "https://atcpmp.sharepoint.com/atcrow/test862"));
+
+            ISharePointCRUD sp = new SharePointCRUD(__appId: appid.Value, __appSecret: apps.Value, _url: "https://atcpmp.sharepoint.com/atcrow/test862");
+            var t = sp.GetSiteTitle();
+            Assert.AreNotEqual("NA", t);
+            Trace.WriteLine(t);
+
+
+            var lists = sp.ListAllLists();
+            Assert.AreNotEqual(0, lists.Count);
+
+            foreach (var l in lists)
             {
                 Trace.WriteLine(l.Title);
             }
