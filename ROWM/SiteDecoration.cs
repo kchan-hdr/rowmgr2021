3c65a07f10723574b1ceb93efe82b1956c542333
tui chan <tui.chan@hotmail.com>
2020-12-10 04:45:05 +0000

    initial build

--- ROWM/SiteDecoration.cs
+++ ROWM/SiteDecoration.cs
@@ -44,4 +44,9 @@
     {
         public string SiteTitle() => "Yoakum SH-72";
     }
+
+    public class Dripping : SiteDecoration
+    {
+        public string SiteTitle() => "City of Dripping Springs";
+    }
 }
