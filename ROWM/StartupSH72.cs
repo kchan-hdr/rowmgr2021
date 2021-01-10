c3ef4debb3ff7fe4fbd574c4cb84f0e6523f1951
Kelly Chan <kchan@hdrinc.com>
2021-01-10 01:31:21 +0000

    initial stand up files

--- ROWM/StartupSH72.cs
+++ ROWM/StartupSH72.cs
@@ -97,9 +97,6 @@ namespace ROWM
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             app.UseExceptionHandler("/Home/Error");
  
             app.UseStaticFiles();
